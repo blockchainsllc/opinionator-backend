e97a6750923d8481ebef046c6fcf132f50d4f2b4
ewf-updater <ewf-updater@ewf.org>
2018-05-22 11:50:17 +0200

    coin voting

--- server.js
+++ server.js
@@ -534,7 +534,8 @@ router.route('/Votes/Gas/:PollId/:ProposalId')
 
     var sum;
     for (let element of addressList.rows) {
-      console.log(parseInt("" + element.address + ": " + await web3.eth.getBalance(element.address)))
+      console.log(addressList.rows)
+      //console.log(parseInt("" + element.address + ": " + await web3.eth.getBalance(element.address)))
       sum = sum + parseInt(await web3.eth.getBalance(element.address))
     }
 
