a32c9b21f2374bd4eeaa28fda680523e09bfa052
Markus Keil <markus.keil@slock.it>
2018-10-18 12:07:30 +0200

    changed everything to mongodb

--- src/database.ts
+++ src/database.ts
@@ -1,4 +1,3 @@
-import {Pool} from 'pg';
 import {BlockChainDatabase} from "./blockChainDatabase";
 
 export interface IDatabaseOptions {
@@ -14,7 +13,6 @@ export interface IDatabaseOptions {
 
 export default class Database {
 
-    private dbPool: Pool;
     private mongoUrl: string;
     private mongoName: string;
 
@@ -22,67 +20,25 @@ export default class Database {
         // Prepare PGSQL connection pool
         this.mongoUrl = opts.mongoUrl;
         this.mongoName = opts.mongoDbName;
-        this.dbPool = new Pool({
-            user: opts.sqlUser,
-            host: opts.sqlHost,
-            database: opts.sqlDatabaseName,
-            password: opts.sqlPassword,
-            port: opts.sqlPort
-        });
     }
 
     public close() {
-        this.dbPool.end();
     }
 
     public async getAllVotes() : Promise<Vote[]> {
-        const resultVotes: Vote[] = [];
-        try {
-            const qry: string = "SELECT id, poll_id, voted_for_proposal, timestamp, address, message FROM votes";
-            const dbResultRows = await this.dbPool.query(qry);
-
-            dbResultRows.rows.forEach(row => {
-                 resultVotes.push({
-                     voteId: row.id,
-                     pollId: row.poll_id,
-                     proposalId: row.voted_for_proposal,
-                     timestamp: row.timestamp,
-                     voterAddress: row.address,
-                     message: row.message,
-                     isValid: true
-                 });
-            });
-
-        }
-        catch(err) {
-            throw "unable to query DB:"  + err;
-        }
-        return resultVotes;
+        const mongo = new BlockChainDatabase(this.mongoUrl,this.mongoName);
+        await mongo.connect();
+        const votes: any[] = await mongo.getVotes(-1);
+        await mongo.close();
+        return votes;
     }
 
     public async getVotesForPoll(pollId: number) : Promise<Vote[]> {
-        const resultVotes: Vote[] = [];
-        try {
-            const qry: string = "SELECT id, voted_for_proposal, timestamp, address, message FROM votes WHERE poll_id = $1";
-            const dbResultRows = await this.dbPool.query(qry,[pollId]);
-
-            dbResultRows.rows.forEach(row => {
-                resultVotes.push({
-                    voteId: row.id,
-                    pollId: pollId,
-                    proposalId: row.voted_for_proposal,
-                    timestamp: row.timestamp,
-                    voterAddress: row.address,
-                    message: row.message,
-                    isValid: true
-                });
-            });
-
-        }
-        catch(err) {
-            throw "unable to query DB:"  + err;
-        }
-        return resultVotes;
+        const mongo = new BlockChainDatabase(this.mongoUrl,this.mongoName);
+        await mongo.connect();
+        const votes: any[] = await mongo.getVotes(pollId);
+        await mongo.close();
+        return votes;
     }
 
     public async getGasSumForAddress(address: string) : Promise<number> {
@@ -94,31 +50,25 @@ export default class Database {
     }
 
     public async checkVoteExists(pollId: number, address: string) :Promise<boolean> {
-        const qry: string = "SELECT count(poll_id) as ct FROM votes WHERE address = $1 AND poll_id = $2";
-        try {
-            const dbResultRows = await this.dbPool.query(qry,[address.toLowerCase(),pollId]);
-            return dbResultRows.rows[0].ct > 0;
-        } catch(err) {
-            throw "Unable to query database: " + err;
-        }
+        const mongo = new BlockChainDatabase(this.mongoUrl,this.mongoName);
+        await mongo.connect();
+        const numVotes: number = await mongo.checkIfVoteExist(pollId,address);
+        await mongo.close();
+        return numVotes > 0;
     }
 
     public async createVote(pollId: number, proposalId: number, address: string, message: string) : Promise<void> {
-        const qry: string = "INSERT INTO votes (poll_id, voted_for_proposal, address, message) VALUES ($1,$2, $3, $4)";
-        try {
-            await this.dbPool.query(qry,[pollId,proposalId,address.toLowerCase(),message]);
-        } catch(err) {
-            throw "Unable to query database: " + err;
-        }
+        const mongo = new BlockChainDatabase(this.mongoUrl,this.mongoName);
+        await mongo.connect();
+        await mongo.createVote(pollId,proposalId,address,message);
+        await mongo.close();
     }
 
     public async updateVote(pollId: number, proposalId: number, address: string, message: string, isValidVote: boolean) : Promise<void> {
-        const qry: string = "UPDATE votes SET voted_for_proposal = $1, message = $2 WHERE poll_id = $3 AND address = $4";
-        try {
-            await this.dbPool.query(qry,[proposalId,message,pollId,address.toLowerCase()])
-        } catch(err) {
-            throw "Unable to query database: " + err;
-        }
+        const mongo = new BlockChainDatabase(this.mongoUrl,this.mongoName);
+        await mongo.connect();
+        await mongo.updateVote(pollId,proposalId,address,message);
+        await mongo.close();
     }
 
     public async getTotalTrxGasForProposal(pollId: number, proposalId: number) : Promise<number> {
@@ -150,17 +100,10 @@ export default class Database {
     }
 
     public async getAddressesForProposal(pollId: number, proposalId: number) : Promise<string[]> {
-        const addresses: string[] = [];
-        const qry: string = 'SELECT address FROM votes WHERE poll_id = $1 AND voted_for_proposal = $2';
-        try {
-            const dbResultRows = await this.dbPool.query(qry,[pollId,proposalId]);
-            dbResultRows.rows.forEach(row => {
-                 addresses.push(row.address);
-            });
-
-        } catch (err) {
-            throw "Unable to query database: " + err;
-        }
+        const mongo = new BlockChainDatabase(this.mongoUrl,this.mongoName);
+        await mongo.connect();
+        const addresses: string[] = await mongo.getAddressForProposal(pollId,proposalId);
+        await mongo.close();
         return addresses;
     }
 
